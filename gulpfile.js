3a0894f3250572968ab5835d71d8ea5b16078b04
JingAn Chen <lzsoft.cja@gmail.com>
2018-01-30 16:03:23 -0500

    Don't process inline import.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 gulp.task('build', function () {
     gulp.src('./src/**/*.css').pipe(cleanCSS({
-        compatibility: '*'
+        compatibility: '*',
+        inline: false
     })).pipe(concat('sui.css')).pipe(gulp.dest('./min/'));
 });
