0343f16e7db148e15142f95e1a9887c2a5a7a5e2
JingAn Chen <lzsoft.cja@gmail.com>
2018-02-13 16:45:48 -0500

    Add a clean script after the install.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
+const del = require('del');
 gulp.task('build', function () {
     gulp.src('./src/**/*.css').pipe(cleanCSS({
         compatibility: '*',
         inline: false
     })).pipe(concat('sui.css')).pipe(gulp.dest('./min/'));
+});
+gulp.task('clean', function () {
+    del('./src/', {
+        force: true
+    });
 });
